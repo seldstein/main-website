455c132ef12476a440603d2af584c9387c1570d2
Sam Feldstein <samuelfeldstein@proton.me>
2023-07-02 12:43:58 -0500

    Add dark mode code (inactive)

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
+// Copyright Date
 var year = new Date().getFullYear();
 document.getElementById("year").innerHTML = year;
+
+// Dark Mode
+const select = document.querySelector("select");
+const html = document.querySelector("html");
+document.body.style.padding = "10px";
+
+function update(bgColor, textColor) {
+    html.style.backgroundColor = bgColor;
+    html.style.color = textColor;
+}
+
+select.addEventListener("change", () =>
+    select.value === "black" ? update("black", "white") : update("white", "black")
+);
